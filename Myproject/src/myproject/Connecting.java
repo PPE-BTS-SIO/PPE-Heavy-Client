e8441b1448777d7fb71823cf14d08d650fbf8d20
Joel <princejoel@hotmail.fr>
2018-03-22 09:47:44 +0100

    nettoyage de classe

--- Myproject/src/myproject/Connecting.java
+++ Myproject/src/myproject/Connecting.java
@@ -12,16 +12,86 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Joel
  */
+//note to self : modify this class so that there's a method for connecting to the database and method for select, insert etc...
 class Connecting {
-    private String url = "jdbc:mysql://localhost:3306/ppe";
-    private String username = "root";
-    private String password = "";
-    private String resultat = "";
+    private Connection connexion = null; 
+    private String url;
+    private String username;
+    private String password;
+    private String resultat;
+    
+    
+    
+    //Constructor 
+    public Connecting() throws SQLException {
+        ouvrirConnexion();
+    }
+    //Initialise les valeurs
+    private boolean ouvrirConnexion() throws SQLException {
+        
+        url = "jdbc:mysql://localhost:3306/ppe";
+        username = "root";
+        password = "";
+        resultat = "";
+        
+        //Ouverture de connexion a la base de donnee
+        try {
+            connexion = DriverManager.getConnection(url, username, password);
+            return true;
+        } catch (SQLException ex) {
+            //Gestion d'eventuelles erreurs
+            switch(ex.getErrorCode()){
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Ne peut pas se connecter au serveur. Contacte l'administrateur", "ERROR 0", JOptionPane.WARNING_MESSAGE );
+                    break;
+                case 1045:
+                    JOptionPane.showMessageDialog(null, "Username ou mot de passe invalid", "ERROR 0", JOptionPane.WARNING_MESSAGE );
+            }
+            
+            
+        }/*finally{
+            
+            if (connexion !=null){
+                try{ 
+                    //Fermeture de la connexion pour ne pas sature le serveur
+                    
+                    System.out.println("reached finally");
+                    connexion.close();
+                }catch(SQLException ignore){
+                    /* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
+               /* }
+            }
+        }*/
+        
+        return true;
+    }
+    //Select Statement 
+    public ResultSet Select(String Query) throws SQLException{       
+       ResultSet resultFinal = null;
+       //Ouvrir connexion
+       if (this.ouvrirConnexion()== true){
+           //creation de l'object gerant les requetes
+           Statement statement = connexion.createStatement();
+           resultFinal = statement.executeQuery(Query);
+           System.out.println("Connection OK");
+       }
+        
+       return resultFinal;
+    }
+    
+    //Update 
+    
+    
+    
+    /*
     public ResultSet connection(String Query) throws SQLException{
          //Ouverture de connexion avec MySQL tournant sur la meme machine
          Connection conn = DriverManager.getConnection(url, username, password);
@@ -34,10 +104,12 @@ class Connecting {
            // resultat = getString(result,"Password");
          }
           System.out.println("proce :" + resultat);
-                 */
+                 *//*
        return result; 
     }
-     
+     */
+    
+    
 }
 
 
